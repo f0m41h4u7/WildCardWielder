6bd4054ce1d41fa6a371de8a7786cf3887d018ab
Anna Berezina <anna.berezina@niuitmo.ru>
2021-03-24 15:56:44 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,35 +8,35 @@ int main()
 {
   try
   {
-	po::options_description desc("WildCardWielder - tool for learning words. Usage:");
-	desc.add_options()
-	  ("help", "produce help message")
-	  ("f", po::value<std::string>(), "provide path to file with cards")
-	  ("r", "set reverse mode")
-	;
+    po::options_description desc("WildCardWielder - tool for learning words. Usage:");
+    desc.add_options()
+      ("help", "produce help message")
+      ("f", po::value<std::string>(), "provide path to file with cards")
+      ("r", "set reverse mode")
+    ;
 
-	po::variables_map vm;
-	po::store(po::parse_command_line(ac, av, desc), vm);
-	po::notify(vm);    
+    po::variables_map vm;
+    po::store(po::parse_command_line(desc), vm);
+    po::notify(vm);    
 
-	if (vm.count("help"))
-	{
-	  std::cout << desc << "\n";
-	  return 1;
-	}
+    if (vm.count("help"))
+    {
+      std::cout << desc << "\n";
+      return 1;
+    }
 
-	std::string fname;
-	if (vm.count("f"))
-	  fname = vm["compression"].as<std::string>();
-	else
-	{
-	  std::cerr << "filename is required as an argument\n";
-	  return 1;
-	}
+    std::string fname;
+    if (vm.count("f"))
+    fname = vm["compression"].as<std::string>();
+    else
+    {
+      std::cerr << "filename is required as an argument\n";
+      return 1;
+    }
 	
-	bool is_reverse = false;
-	if (vm.count("f"))
-		is_reverse = true;
+    bool is_reverse = false;
+    if (vm.count("f"))
+      is_reverse = true;
     
     wcw::WildCardWielder w{fname, is_reverse};
     if(!w.deserialize()) return 1;
